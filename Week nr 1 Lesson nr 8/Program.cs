7461407741765bc06d5243d355923eb5f7c9c1a6
Ilka <Ilka@LAPTOP-VAVOB6QC>
2023-05-30 19:09:30 +0200

    Add project files.

---
+++ Week nr 1 Lesson nr 8/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Week_nr_1_Lesson_nr_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Cześć Świecie C#");
+            Console.WriteLine("Nazywam się Elen");
+            Console.Write("It is lesson number 8, the first week");
+        }
+    }
+}
